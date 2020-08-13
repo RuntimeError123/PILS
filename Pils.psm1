230befd83cca6a54a04b9d5fea4676483633de45
RuntimeError123 <36066761+RuntimeError123@users.noreply.github.com>
2020-08-13 11:48:03 +0200

    Initial commit

---
+++ Pils.psm1
@@ -0,0 +1,68 @@
+function WriteLog
+{
+    param(
+        [Parameter(ParameterSetName="Message",Mandatory=$true)]
+        [String]$Message,
+        [Parameter(ParameterSetName="Message",Mandatory=$true)][ValidateSet('Information','Warning','Error')]
+        [String]$Severity,
+        [Parameter(ParameterSetName="CustomMessage",Mandatory=$true)]
+        [Switch]$CustomMessage
+    )
+
+    $Date = Get-Date
+    $DateMessage = $Date | Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+    $DateLog = $Date | Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $DateLogFileName = $Date | Get-Date -Format "yyyyMMdd"
+
+    if ($PILSLogFilePrefix)
+    {
+        $LogFilePrefix = $PILSLogFilePrefix
+    }
+    else 
+    {
+        $LogFilePrefix = "PILS"    
+    }
+
+    if ($PILSLogFolder)
+    {
+        $LogFolder = $PILSLogFolder
+    }
+    else 
+    {
+        $LogFolder = $env:TEMP
+    }
+
+    $LogFilePath = Join-Path -Path $LogFolder -ChildPath ($LogFilePrefix + "-" + $DateLogFileName + ".txt")
+
+    if($Message)
+    {
+        Write-Host "[$DateMessage] " -NoNewline
+        Write-Host "[" -NoNewline
+        switch ($Severity)
+        {
+            "Information"
+            {
+                Write-Host -ForegroundColor Cyan "Information" -NoNewline
+            }
+            "Warning"
+            {
+                Write-Host -ForegroundColor Yellow "Warning    " -NoNewline
+            }
+            "Error"
+            {
+                Write-Host -ForegroundColor Red "Error      " -NoNewline
+            }
+        }
+        Write-Host "] " -NoNewline
+        Write-Host $Message
+        "[$DateLog] - $Severity - $Message" | Out-File -FilePath $LogFilePath -Append
+    }
+    
+    if($CustomMessage)
+    {
+        Write-Output "[$DateMessage] $Message
+        "[$DateLog] $Message | Out-File -FilePath $LogFilePath -Append
+        
+    }
+}
+
