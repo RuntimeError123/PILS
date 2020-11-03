7537180934e9ee5f5cff3d32ce122de3424be2da
RuntimeError123 <36066761+RuntimeError123@users.noreply.github.com>
2020-11-03 21:37:07 +0100

    Added begin, process and end block

--- Pils.psm1
+++ Pils.psm1
@@ -9,60 +9,66 @@ function WriteLog
         [Switch]$CustomMessage
     )
 
-    $Date = Get-Date
-    $DateMessage = $Date | Get-Date -Format "dd-MM-yyyy HH:mm:ss"
-    $DateLog = $Date | Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $DateLogFileName = $Date | Get-Date -Format "yyyyMMdd"
+    begin 
+    {
+        $Date = Get-Date
+        $DateMessage = $Date | Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+        $DateLog = $Date | Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $DateLogFileName = $Date | Get-Date -Format "yyyyMMdd"
 
-    if ($PILSLogFilePrefix)
-    {
-        $LogFilePrefix = $PILSLogFilePrefix
-    }
-    else 
-    {
-        $LogFilePrefix = "PILS"    
-    }
-
-    if ($PILSLogFolder)
-    {
-        $LogFolder = $PILSLogFolder
-    }
-    else 
-    {
-        $LogFolder = $env:TEMP
-    }
-
-    $LogFilePath = Join-Path -Path $LogFolder -ChildPath ($LogFilePrefix + "-" + $DateLogFileName + ".txt")
-
-    if($Message)
-    {
-        Write-Host "[$DateMessage] " -NoNewline
-        Write-Host "[" -NoNewline
-        switch ($Severity)
+        if ($PILSLogFilePrefix)
         {
-            "Information"
-            {
-                Write-Host -ForegroundColor Cyan "Information" -NoNewline
-            }
-            "Warning"
-            {
-                Write-Host -ForegroundColor Yellow "Warning    " -NoNewline
-            }
-            "Error"
-            {
-                Write-Host -ForegroundColor Red "Error      " -NoNewline
-            }
+            $LogFilePrefix = $PILSLogFilePrefix
         }
-        Write-Host "] " -NoNewline
-        Write-Host $Message
-        "[$DateLog] - $Severity - $Message" | Out-File -FilePath $LogFilePath -Append
+        else 
+        {
+            $LogFilePrefix = "PILS"    
+        }
+
+        if ($PILSLogFolder)
+        {
+            $LogFolder = $PILSLogFolder
+        }
+        else 
+        {
+            $LogFolder = $env:TEMP
+        }
+
+        $LogFilePath = Join-Path -Path $LogFolder -ChildPath ($LogFilePrefix + "-" + $DateLogFileName + ".txt")
     }
-    
-    if($CustomMessage)
+    process
     {
-        Write-Output "[$DateMessage] $Message
-        "[$DateLog] $Message | Out-File -FilePath $LogFilePath -Append
+        if($Message)
+        {
+            Write-Host "[$DateMessage] " -NoNewline
+            Write-Host "[" -NoNewline
+            switch ($Severity)
+            {
+                "Information"
+                {
+                    Write-Host -ForegroundColor Cyan "Information" -NoNewline
+                }
+                "Warning"
+                {
+                    Write-Host -ForegroundColor Yellow "Warning    " -NoNewline
+                }
+                "Error"
+                {
+                    Write-Host -ForegroundColor Red "Error      " -NoNewline
+                }
+            }
+            Write-Host "] " -NoNewline
+            Write-Host $Message
+            "[$DateLog] - $Severity - $Message" | Out-File -FilePath $LogFilePath -Append
+        }
         
+        if($CustomMessage)
+        {
+            Write-Output "[$DateMessage] $Message
+            "[$DateLog] $Message | Out-File -FilePath $LogFilePath -Append
+            
+        }
     }
+    end {}
 }
 
